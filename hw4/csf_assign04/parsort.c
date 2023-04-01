21fd85049f467ea092b710f80fb9922e5fe6f13d
root <andy.jaramillo022@gmail.com>
2023-03-31 20:48:37 -0400

    added hw4. the base is there, but merge_sort is not working sequentially

---
+++ hw4/csf_assign04/parsort.c
@@ -0,0 +1,127 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare_i64(long int * left, long int * right){
+  if(left == right){
+    return 0;
+  } else if(left < right){
+    return -1;
+  } else{
+    return 1;
+  }
+}
+
+
+// Merge the elements in the sorted ranges [begin, mid) and [mid, end),
+// copying the result into temparr.
+void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr) {
+  int64_t *endl = arr + mid;
+  int64_t *endr = arr + end;
+  int64_t *left = arr + begin, *right = arr + mid, *dst = temparr;
+
+  for (;;) {
+    int at_end_l = left >= endl;
+    int at_end_r = right >= endr;
+
+    if (at_end_l && at_end_r) break;
+
+    if (at_end_l)
+      *dst++ = *right++;
+    else if (at_end_r)
+      *dst++ = *left++;
+    else {
+     int cmp = compare_i64(left, right);
+     if (cmp <= 0)
+       *dst++ = *left++;
+     else
+       *dst++ = *right++;
+    }
+  }
+}
+
+
+void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
+  if(begin < end){
+    int mid = begin + (end - begin) / 2;
+    merge_sort(arr, begin, mid, threshold);
+    merge_sort(arr, mid + 1, end, threshold);
+    merge(arr, begin, mid, end, arr);
+  }
+}
+
+int main(int argc, char **argv) {
+  // check for correct number of command line arguments
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <filename> <sequential threshold>\n", argv[0]);
+    return 1;
+  }
+
+  // process command line arguments
+  const char *filename = argv[1];
+  char *end;
+  size_t threshold = (size_t) strtoul(argv[2], &end, 10);
+  if (end != argv[2] + strlen(argv[2])){
+    fprintf(stderr, "%ld Not high enough threshhold", strlen(argv[2]));
+    return 1;
+  }
+    /* TODO: report an error (threshold value is invalid) */;
+  
+  // TODO: open the file
+
+  // TODO: use fstat to determine the size of the file
+  int fd = open(filename, O_RDWR);
+if (fd < 0) {
+  fprintf(stderr, "Unsupported file");
+  return 1;
+}
+
+// TODO: map the file into memory using mmap
+
+struct stat statbuf;
+int rc = fstat(fd, &statbuf);
+if (rc != 0) {
+   fprintf(stderr, "unable to get file information");
+   return 1;
+}
+size_t file_size_in_bytes = statbuf.st_size;
+
+int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+// you should immediately close the file descriptor here since mmap maintains a separate
+// reference to the file and all open fds will gets duplicated to the children, which will
+// cause fd in-use-at-exit leaks.
+// TODO: call close()
+
+if (data == MAP_FAILED) {
+   fprintf(stderr, "unable to map using mmap");
+   return 1;
+}
+// *data now behaves like a standard array of int64_t. Be careful though! Going off the end
+// of the array will silently extend the file, which can rapidly lead to disk space
+// depletion!
+  // TODO: sort the data!
+    merge_sort(data, 0, file_size_in_bytes/sizeof(int64_t), threshold);
+  // TODO: unmap and close the file
+    int ummap = munmap(data, file_size_in_bytes);
+
+    if(ummap != 0){
+      fprintf(stderr, "unable to unmap with munmap");
+   return 1;
+    }
+    int closed = close(fd);
+    if(closed != 0){
+      fprintf(stderr, "unable to close the file");
+      return 1;
+    }
+
+  // TODO: exit with a 0 exit code if sort was successful
+  return 0;
+}
