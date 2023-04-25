90fbbf27001ae00f50ea11008b3a644246e06c6f
oween12 <owen.bianchi12@gmail.com>
2023-04-25 15:11:20 -0400

    cleaned up syntax

--- hw5/csf_assign05/connection.cpp
+++ hw5/csf_assign05/connection.cpp
@@ -14,13 +14,10 @@ Connection::Connection()
 Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
   rio_readinitb(&m_fdbuf, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
-  // TODO: call rio_readinitb to initialize the rio_t object
   const char* hostname_c_str = hostname.c_str();
   std::string port_string = std::to_string(port);
   const char* port_c_str = port_string.c_str();
@@ -33,12 +30,10 @@ void Connection::connect(const std::string &hostname, int port) {
 }
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
   Close(m_fd);
 }
 
 bool Connection::is_open() const {
-  // TODO: return true if the connection is open
   if(m_fd < 0) {
     return false;
   }
@@ -46,14 +41,10 @@ bool Connection::is_open() const {
 }
 
 void Connection::close() {
-  // TODO: close the connection if it is open
   Close(m_fd);
 }
 
 bool Connection::send(const Message &msg) {
-  // TODO: send a message
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
   std::string tag = msg.tag;
   std::string data = msg.data;
   std::string total = tag + ":" + data + "\n";
@@ -67,26 +58,14 @@ bool Connection::send(const Message &msg) {
 }
 
 bool Connection::receive(Message &msg) {
-  // TODO: receive a message, storing its tag and data in msg
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
   char buf[MAXLINE];
   ssize_t rc = rio_readlineb(&m_fdbuf, buf, sizeof(buf));
 
   if (rc <= 0) {
     m_last_result = Result(EOF_OR_ERROR);
-  return false;// error reading data from client
+    return false;
   } else {
-    //  std::string s(buf);
-    // std::stringstream ss(s);
-    // std::string tag;
-    // std::string data;
-    // getline(ss, tag, ':');
-    // getline(ss, data);
-    // Message *mes = new Message(tag, data);
- //   return mes;
     msg = *(msg.bufferToMessage(buf));
-
     return true;
   }
 }
